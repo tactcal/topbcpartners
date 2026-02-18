55e64a390bddcb68247d934141f4f0c7d351cc15
Kris Ruyeras <Kristoffer@matalino.io>
2026-02-17 18:00:02 -0800

    Comment sitemap.xml reference

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/admin/', // Keeps your admin portal private from Google
     },
-    //sitemap: 'https://topbcpartners.com/sitemap.xml',
+    //sitemap: 'https://topbcpartners.com/sitemap.xml',/
   };
 }
